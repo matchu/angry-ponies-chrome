799649807d098687e4ffdb5ab36d00f65374cdba
Matchu <matchu1993@gmail.com>
2012-04-30 13:19:24 -0500

    well, here's a version that proxies everything to my server

--- analyze_game.js
+++ analyze_game.js
@@ -1,7 +1,32 @@
-(function () {
-    console.log("I am analyzing the game.");
+/*(function () {
+    var HOST = document.head.getElementsByTagName("base")[0].href;
+    chrome.extension.sendRequest({listenToHost: HOST});
+    
+    function skinXHR() {
+        var SPRITE_SHEET_NAME = "INGAME_BIRDS";
+        var REMOTE_DIR = "cors/fowl/sprite_sheets/";
+        var REMOTE_SPRITE_MANIFEST_SRC = REMOTE_DIR + SPRITE_SHEET_NAME + ".json";
+        var GHOST_ASSET_DIR = "http://www.matchusian.com/mlp_sprite_sheets/";
+        
+        var xhr = window.XMLHttpRequest.prototype;
+        xhr.unskinnedOpen = xhr.open;
+        xhr.open = function (method, url, sync) {
+            if(url == REMOTE_SPRITE_MANIFEST_SRC) {
+                url = GHOST_ASSET_DIR + SPRITE_SHEET_NAME + ".json";
+                console.log("ponified to " + url);
+            }
+            return this.unskinnedOpen(method, url, sync);
+        }
+        console.log("skinned");
+    }
+    
+    console.log("will skin");
+    var script = document.createElement("script");
+    script.innerHTML = skinXHR.toString() + "; skinXHR();";
+    document.body.appendChild(script);
+})();*/
 
-    var assetHost = document.head.getElementsByTagName('base')[0].href;
-    chrome.extension.sendRequest({listenToHost: assetHost});
-    console.log("Let's listen to " + assetHost);
+(function () {
+    var base = document.head.getElementsByTagName("base")[0];
+    base.href = "http://www.matchusian.com/mlp_sprite_sheets/";
 })();
